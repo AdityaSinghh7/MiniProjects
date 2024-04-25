741b2341e31b9b323a1211dc54f0f5ad7e0b2c29
Aditya Dev Singh <adityads@uci.edu>
2024-04-25 09:21:50 -0700

    Resume Changed

--- PersonalPortfolio/ResumePage/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var downloadButton = document.createElement('button');
-    downloadButton.textContent = 'Download Resume';
-    downloadButton.onclick = function () {
-        window.location.href = 'Resume_Aditya.pdf';
-    };
-
-    document.body.appendChild(downloadButton);
-};
